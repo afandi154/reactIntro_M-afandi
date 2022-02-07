8149eb4c0b2ac697e711e8ee48a48d428d2aa387
afandi154 <mahfudhafandi23@gmail.com>
2022-02-07 20:55:53 +0700

    Updating React App with Props and State

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Buttons from './components/Buttons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <strong>Hello World Eduwork !</strong>
-          <br/>
-          <em>- Mahfudh Afandi</em>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  styles = {
+    width: "80%",
+    margin: "auto",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottom: "2px solid black"
+  }
+  render() {
+    return (
+      <div style={this.styles}>
+        <h1>Hello {this.props.input}</h1>
+        <Buttons />
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App input={prompt("Masukan nama anda")}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
